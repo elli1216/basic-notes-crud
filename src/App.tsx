199770caa554148cf0cd9b4d4664cb0b42a29ea0
Darl Ellison <floresca.darlellisong@gmail.com>
2025-03-02 14:58:21 +0800

    changed yarn to pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, Routes, Route } from 'react-router-dom';
-import Header from './components/header';
-import AddNewNote from './components/addNewNote';
-import EditNote from './components/editNote';
-import CirclePlus from './assets/circle-plus.svg';
-import PenToSquare from './assets/pen-to-square-solid.svg';
-import Trashcan from './assets/trash-can-solid.svg';
-import { fetchNotes, deleteNote } from './api/fetcher';
-import { Note } from './types/note';
+import { useEffect, useState } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import AddNewNote from "./components/addNewNote";
+import EditNote from "./components/editNote";
+import CirclePlus from "./assets/circle-plus.svg";
+import PenToSquare from "./assets/pen-to-square-solid.svg";
+import Trashcan from "./assets/trash-can-solid.svg";
+import { fetchNotes, deleteNote } from "./api/fetcher";
+import { Note } from "./types/note";
 
 function App() {
   const [data, setData] = useState<Note[]>([]);
@@ -39,10 +39,10 @@ function App() {
 
   const deleteNoteFromList = async (id: number | undefined) => {
     try {
-      await deleteNote(id? id : 0);
+      await deleteNote(id ? id : 0);
       setData((prevData) => prevData.filter((note) => note.id !== id));
     } catch (error) {
-      console.error('Error deleting note:', error);
+      console.error("Error deleting note:", error);
     }
   };
 
@@ -53,17 +53,31 @@ function App() {
 
   const renderNotes = () => {
     return (
-      <ul className='grid grid-flow-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2 p-2'>
+      <ul className="grid grid-flow-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2 p-2">
         {data.map((note) => (
           <li
-          className='note-item text-center p-2 bg-grey border-2 m-2 pb-20 rounded-md shadow-2xl min-w-50'
-          key={note.id}
-        >
-          <h2 className='text-center p-2 text-3xl font-bold w-fit'>{note.title}</h2>
-          <p className='text-2xl'>{note.content}</p>
-          <div className='note-actions flex justify-center gap-4 my-3'>
-            <img className='hover:cursor-pointer' src={PenToSquare} alt="Edit" width={'30rem'} onClick={() => handleNoteClick(note)}/>
-            <img className='hover:cursor-pointer' src={Trashcan} alt="Delete" width={'30rem'} onClick={() => deleteNoteFromList(note.id)}/>
+            className="note-item text-center p-2 bg-grey border-2 m-2 pb-20 rounded-md shadow-2xl min-w-50"
+            key={note.id}
+          >
+            <h2 className="text-center p-2 text-3xl font-bold w-fit">
+              {note.title}
+            </h2>
+            <p className="text-2xl">{note.content}</p>
+            <div className="note-actions flex justify-center gap-4 my-3">
+              <img
+                className="hover:cursor-pointer"
+                src={PenToSquare}
+                alt="Edit"
+                width={"30rem"}
+                onClick={() => handleNoteClick(note)}
+              />
+              <img
+                className="hover:cursor-pointer"
+                src={Trashcan}
+                alt="Delete"
+                width={"30rem"}
+                onClick={() => deleteNoteFromList(note.id)}
+              />
             </div>
           </li>
         ))}
@@ -72,11 +86,15 @@ function App() {
   };
 
   return (
-    <div className='absolute w-full h-full bg-gray-200'>
+    <div className="absolute w-full h-full bg-gray-200">
       <Header />
       {renderNotes()}
       {showAddNewNote && (
-        <AddNewNote setShowAddNewNote={setShowAddNewNote} addNewNoteToList={addNewNoteToList} note={{ title: '', content: '' }}/>
+        <AddNewNote
+          setShowAddNewNote={setShowAddNewNote}
+          addNewNoteToList={addNewNoteToList}
+          note={{ title: "", content: "" }}
+        />
       )}
       {currentNote && (
         <Routes>
@@ -84,7 +102,7 @@ function App() {
             path="/edit/:id"
             element={
               <EditNote
-                setShowEditNote={() => navigate('/')}
+                setShowEditNote={() => navigate("/")}
                 note={currentNote}
                 updateNoteInList={updateNoteInList}
               />
@@ -93,7 +111,7 @@ function App() {
         </Routes>
       )}
       <img
-        className='w-12 hover:cursor-pointer fixed bottom-4 right-4'
+        className="w-12 hover:cursor-pointer fixed bottom-4 right-4"
         src={CirclePlus}
         alt="circle-plus"
         onClick={() => setShowAddNewNote(!showAddNewNote)}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Main from "./main";
+import Main from "./Main";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
